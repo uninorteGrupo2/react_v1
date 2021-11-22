79a418c7fd313dc5026566ec741544c5205a4a92
unknown <miemail@dominio.com>
2021-11-22 09:44:53 -0500

    react v2

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = ()=>{
+    return(
+        <div>
+            <h1>Hola estoy en React</h1>
+            <a href="https://www.youtube.com/">YOUTUBE</a>
+        </div>
+        
+    )
+}
+
+export default App;
